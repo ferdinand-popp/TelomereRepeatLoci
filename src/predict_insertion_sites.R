f91e1d999466a6548546ad52937dc18328d18d03
Ferdinand Popp <f.popp@dkfz.de>
2026-07-08 13:12:41 +0200

    add comments

--- src/predict_insertion_sites.R
+++ src/predict_insertion_sites.R
@@ -96,6 +96,10 @@ ratio_or_na = function(num, den){
 
 for(window in unique(clipped_reads_all$window)){
 
+  #-------------------------------------------------------------------------------------------------------------------
+  # get most likely insertion site (where do the most clipped sequences start or end?)
+  #-------------------------------------------------------------------------------------------------------------------
+
   clipped_reads = clipped_reads_all[clipped_reads_all$window==window, ]
 
   # defaults
@@ -111,11 +115,26 @@ for(window in unique(clipped_reads_all$window)){
   }
 
   clipped_reads_filtered = clipped_reads[clipped_reads$part_telomere, ]
+    
+  #-----------------------------------------------------------------------------------
+  # discordant reads on plus strand => clipped reads should end at same position
+  #
+  # 5' ------ chromosome ------ telomere 3'
+  #
+  #-----------------------------------------------------------------------------------
 
   if (compareNA(candidate_regions[window, "strand"], "+")){
     clipped_expected_pos_fusion = "downstream"
     clipped_start_end = "end"
     site_offset = 1
+
+  #-----------------------------------------------------------------------------------
+  # discordant reads on minus strand => clipped reads should start at same position
+  #
+  # 5' telomere ------ chromosome ------ 3'
+  #
+  #-----------------------------------------------------------------------------------  
+  
   }else if (compareNA(candidate_regions[window, "strand"], "-")){
     clipped_expected_pos_fusion = "upstream"
     clipped_start_end = "start"
@@ -124,8 +143,18 @@ for(window in unique(clipped_reads_all$window)){
     site_offset = NA
   }
 
+  #-------------------------------------------------------------------------
+  # only keep clipped reads that match orientation of discordant reads
+  #-------------------------------------------------------------------------
+
+  #attention: does not consider reads that are clipped on both ends (these are unlikely to be indicative of telomere insertion)
   clipped_reads_filtered_matching_discordant = clipped_reads_filtered[compareNA(clipped_reads_filtered$expected_pos_fusion, clipped_expected_pos_fusion), ]
 
+  #-------------------------------------------------------------------------  
+  # only keep clipped reads that match position of discordant reads
+  #-------------------------------------------------------------------------
+  
+  # currently taking median to prevent missing insertions where there is another discordant read nearby       
   chrom = candidate_regions[window, "chrom"]
   window_start = candidate_regions[window, "chromStart"]
   window_end = candidate_regions[window, "chromEnd"]
@@ -135,7 +164,7 @@ for(window in unique(clipped_reads_all$window)){
                                            discordant_read_table$mate_position<= window_end &
                                            discordant_read_table$mate_strand==candidate_regions[window, "strand"], ]
 
-  discordant_read_pos_median = median(discordant_reads$mate_position) + 50
+  discordant_read_pos_median = median(discordant_reads$mate_position) + 50 # plus 50 to get middle of read
 
   if (compareNA(candidate_regions[window, "strand"], "+")){
     clipped_reads_filtered_matching_discordant = clipped_reads_filtered_matching_discordant[clipped_reads_filtered_matching_discordant$end>discordant_read_pos_median, ]
@@ -143,6 +172,11 @@ for(window in unique(clipped_reads_all$window)){
     clipped_reads_filtered_matching_discordant = clipped_reads_filtered_matching_discordant[clipped_reads_filtered_matching_discordant$start<discordant_read_pos_median, ]
   }
 
+
+  #------------------------------------------------------
+  # where do most reads start/end?
+  #------------------------------------------------------
+  
   table_pos_insertion = as.data.frame(table(clipped_reads_filtered_matching_discordant[, clipped_start_end]), stringsAsFactors=FALSE)
 
   if (dim(table_pos_insertion)[1] != 0){
@@ -162,6 +196,11 @@ for(window in unique(clipped_reads_all$window)){
     candidate_regions[window, "pos_telomeres_from_insertion"] = clipped_expected_pos_fusion
     candidate_regions[window, "reads_supporting_insertion_pos"] = max(table_pos_insertion$unique_cigars)
 
+    #-----------------------------------------------------------------------------------
+    # get total TTAGGG and CCCTAA counts in telomere fusion reads at insertion site
+    # and determine most likely repeat on forward strand
+    #-----------------------------------------------------------------------------------
+      
     clipped_reads_filtered_at_insertion = clipped_reads_filtered_matching_discordant[clipped_reads_filtered_matching_discordant[, clipped_start_end]==insertion_pos,]
     sum_TTAGGG_count = sum(clipped_reads_filtered_at_insertion$TTAGGG_count)
     sum_CCCTAA_count = sum(clipped_reads_filtered_at_insertion$CCCTAA_count)
